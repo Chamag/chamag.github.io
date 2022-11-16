a5fd6f6973faadd212ac56b70bce27cc18d78a3d
Chamag <jfsaffert@gmail.com>
2022-11-16 15:35:06 +0100

    initial commit

---
+++ kompetenzen/index.js
@@ -0,0 +1,21 @@
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+};
+
+window.onscroll = function() {
+	myFunction()
+};
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+	if (window.pageYOffset >= sticky) {
+		navbar.classList.add("sticky");
+		document.getElementById("mobile-menu").style.top = "80px";
+	} else {
+		navbar.classList.remove("sticky");
+		document.getElementById("mobile-menu").style.top = "120px";
+	}
+}
