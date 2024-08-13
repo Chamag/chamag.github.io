b97d43db968161fd195c4ac1638eb35bc4852217
Chamag <jfsaffert@gmail.com>
2024-08-13 19:00:11 +0200

    Language change test

--- js/script.js
+++ js/script.js
@@ -1,29 +1,3 @@
-/*function toggleMobileMenu(menu) {
-    menu.classList.toggle('open');
-    let mobileNav = document.getElementById("mobile-nav");
-    let hambIconBar1 = document.getElementById("bar1");
-    let hambIconBar2 = document.getElementById("bar2");
-    let hambIconBar3 = document.getElementById("bar3");
-    if (mobileNav.style.visibility === "hidden") {
-    	mobileNav.style.visibility = "visible";
-    	mobileNav.style.opacity = 1;
-    	if (this.scrollY < 51) {
-    		document.getElementById("header-title").style.display = "block";
-    	}
-        hambIconBar1.style.backgroundColor = "#000000";
-        hambIconBar2.style.backgroundColor = "#000000";
-        hambIconBar3.style.backgroundColor = "#000000";
-    } else {
-    	mobileNav.style.visibility = "hidden";
-    	mobileNav.style.opacity = 0;
-    	if (this.scrollY < 51) {
-    		document.getElementById("header-title").style.display = "none";
-    	}
-        hambIconBar1.style.backgroundColor = "#000000";
-        hambIconBar2.style.backgroundColor = "#000000";
-        hambIconBar3.style.backgroundColor = "#000000";
-    }
-}*/
 
 function collapseInfo(collapsible) {
     collapsible.classList.toggle("active");
@@ -62,6 +36,15 @@ function collapseInfo4(collapsible) {
     }
 }
 
+function toggleLanguage() {
+    let texten = document.getElementsByClassName("textEN")
+    let textde = document.getElementsByClassName("textDE")
+    for (let i = 0; i < texten.length; i++) {
+        texten[i].style.display = block;
+        textde[i].style.display = none;
+    }
+}
+
 function scrollActions() {
 	var headerEle = document.getElementById("header");
 	this.scrollY > 50 ? headerEle.style.background = "#FFFFFF" : headerEle.style.background = "";
@@ -74,12 +57,6 @@ function scrollActions() {
     if (window.getComputedStyle(deskNav, null).display === "block") {
 		document.getElementById("header-title").innerText = "";
     }
-    //let hambIconBar1 = document.getElementById("bar1");
-    //let hambIconBar2 = document.getElementById("bar2");
-    //let hambIconBar3 = document.getElementById("bar3");
-    //this.scrollY > 50 ? hambIconBar1.style.backgroundColor = "#0E6837" : hambIconBar1.style.backgroundColor ="";
-    //this.scrollY > 50 ? hambIconBar2.style.backgroundColor = "#0E6837" : hambIconBar2.style.backgroundColor ="";
-    //this.scrollY > 50 ? hambIconBar3.style.backgroundColor = "#0E6837" : hambIconBar3.style.backgroundColor ="";
 }
 
 window.addEventListener("scroll", scrollActions, false);
